d7ef4ac9e6873fab3208adc1e758cae90e620e10
Ariel Alejandro Gálvez Mirabal <144627936+Arielalegm@users.noreply.github.com>
2025-03-13 16:27:36 -0400

    Actualizar config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 const CONFIG = {
     GEMINI_API_KEY: 'AIzaSyCbMUwwdbL0ruej2M2N8aC5VApG5LHJZwI',
-    MODEL_NAME: 'gemini-1.5-flash',
+    MODEL_NAME: 'gemini-2.0-flash',
     SYSTEM_PROMPT: `Sigue estas reglas:
 1. Responde siempre en español
-2. Tus respuestas deben ser precisas y sin divagaciones`
+2. Tus respuestas deben ser precisas y sin divagaciones
+3. eres un asistente de IA en WhatsApp`
 };
 
 module.exports = CONFIG;
